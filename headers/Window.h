1cff33b327462e3e84b4a625f9db11af35ee5294
Yu Huang <y.huang0112@gmail.com>
2022-06-18 20:07:56 +0200

    Deleted copy constructor

--- headers/Window.h
+++ headers/Window.h
@@ -2,13 +2,14 @@
 #define WINDOW_H
 #include <SDL2/SDL.h>
 
-#define WIN_WIDTH 640
-#define WIN_HEIGHT 720
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 720
 
 class Window{
 private:
   static SDL_Window* win;
   Window();
+  Window(const Window& w) = delete;
 public:
   static SDL_Window* get_window();
   static void destroy_window();
