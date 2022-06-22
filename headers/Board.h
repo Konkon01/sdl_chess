ac52574754fbcd6b7d3e321a6dfb62cc8542b8fd
Yu Huang <y.huang0112@gmail.com>
2022-06-22 21:46:02 +0200

    Added methods to get index from coordinates and vice versa

--- headers/Board.h
+++ headers/Board.h
@@ -1,10 +1,14 @@
 #ifndef BOARD_H
 #define BOARD_H
+#include <utility>
 
 class Board {
   char board[64];
 public:
   Board();
+  
+  std::pair<int, int> get_coord_from_ind(int i);
+  int get_ind_from_coord(std::pair<int, int> coord);
 };
 
 #endif
--- src/Board.cpp
+++ src/Board.cpp
@@ -1,5 +1,7 @@
 #include "../headers/Board.h"
 
+#include <utility>
+
 Board::Board(){
     /*         STARTING CHESSBOARD
      'r', 'h', 'b', 'q', 'k', 'b', 'h', 'r',
@@ -36,4 +38,25 @@ Board::Board(){
     board[61] = 'B';
     board[62] = 'H';
     board[63] = 'R';
+}
+
+std::pair<int, int> Board::get_coord_from_ind(int i){
+  std::pair<int, int> coord;
+  
+  //Upper left corner of the rectangle
+  coord.first = i % 8;
+  coord.second = i / 8;
+  
+  return coord;
+}
+
+int Board::get_ind_from_coord(std::pair<int, int> coord){
+  int i;
+  
+  // 1/8 of the screen width 80px
+  int x = coord.first / 80;
+  int y = coord.second / 80;
+
+  i = y * 7 + x;
+  return i;
 }
