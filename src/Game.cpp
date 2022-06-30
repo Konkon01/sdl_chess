cc428044631d8f5ffb5ef6af260a4bbd31dd3c2c
Yu Huang <y.huang0112@gmail.com>
2022-06-30 09:45:41 +0200

    Change of window width and height

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,24 +1,20 @@
 #include "../headers/Game.h"
-#include "../headers/Window.h"
 #include "../headers/EventHandler.h"
 #include "../headers/Board.h"
 #include "../headers/Renderer.h"
+#include "../headers/Field.h"
 
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <vector>
-
-
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 720
 
 Game::Game(){
   SDL_Init(SDL_INIT_EVERYTHING);
   window = SDL_CreateWindow("Chess Game",
                           SDL_WINDOWPOS_CENTERED,
                           SDL_WINDOWPOS_CENTERED,
-                          SCREEN_WIDTH,
-                          SCREEN_HEIGHT,
+                          FIELD_WIDTH * 8,
+                          FIELD_HEIGHT * 8,
                           SDL_WINDOW_SHOWN);
   board = new Board();
   event_handler = new EventHandler(board);
@@ -39,8 +35,10 @@ void Game::start_game(){
 
   SDL_Event event = event_handler->get_event();
 
+  std::vector<int> possible_moves;
+  renderer->render_board(possible_moves);
+
   while (SDL_WaitEvent(&event)){
-    std::vector<int> possible_moves;
     if (event.type == SDL_QUIT){
       break;
     
@@ -48,7 +46,6 @@ void Game::start_game(){
       event_handler->handle_events(possible_moves);
 
     }
-    renderer->render_board(possible_moves);
   }
 
   clean_up();
