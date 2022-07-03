9d89e6589673658c2419482ebe81d139216643ad
Yu Huang <y.huang0112@gmail.com>
2022-07-03 22:06:21 +0200

    Started event handling

--- headers/EventHandler.h
+++ headers/EventHandler.h
@@ -8,7 +8,12 @@ class EventHandler {
 private:
   SDL_Event event;
   Board* board;
-  bool is_running;
+  bool is_light_turn;
+  char selected_piece;
+  int selected_ind;
+
+  void reset(int &s_ind, char &s_piece, std::vector<int> &possible_moves);
+  bool is_possible(std::vector<int> &possible_moves, int field_ind);
 public:
   EventHandler(Board* b);
   SDL_Event& get_event();
--- src/EventHandler.cpp
+++ src/EventHandler.cpp
@@ -1,14 +1,34 @@
 #include "../headers/EventHandler.h"
 #include "../headers/Board.h"
+
 #include <SDL2/SDL.h>
+#include <utility>
 #include <iostream>
 
 EventHandler::EventHandler(Board* b){
   board = b;
+  is_light_turn = true;
+  selected_piece = 'x';
+  selected_ind = -1;
 }
 
 SDL_Event& EventHandler::get_event(){
   return event;
+}
+
+void EventHandler::reset(int &s_ind, char &s_piece, std::vector<int> &possible_moves){
+  s_ind = -1;
+  s_piece = -1;
+  possible_moves.clear();
+}
+
+bool EventHandler::is_possible(std::vector<int> &possible_moves, int field_ind){
+  for(int p_move : possible_moves){
+    if(p_move == field_ind){
+      return true;
+    }
+  }
+  return false;
 }
 
 void EventHandler::handle_events(std::vector<int> &possible_moves, bool &running){
@@ -19,6 +39,44 @@ void EventHandler::handle_events(std::vector<int> &possible_moves, bool &running
       running = false;
       break;
     }
+
+    if(event.type == SDL_MOUSEBUTTONDOWN){
+      std::pair<int, int> coord;
+      coord.first = event.button.x;
+      coord.second = event.button.y;
+
+      int field_ind = board->get_ind_from_coord(coord);
+      char field_piece = board->get_piece_at(field_ind);
+
+      if(selected_ind != -1){
+        /*TASKS
+          -new field is in possible_moves vector?
+          -was check given after move?
+          -game over after last move?
+        */
+        if(selected_ind == field_ind){
+          reset(selected_ind, selected_piece, possible_moves);
+          break;
+        }
+
+        if(is_possible(possible_moves, field_ind)){ //Valid move
+          board->move_piece(selected_ind, field_ind);
+        }
+        
+        reset(selected_ind, selected_piece, possible_moves);
+        break;
+    
+      } else {
+        /*TASKS
+          -select piece
+          -calculate possible moves
+        */
+        selected_ind = field_ind;
+        selected_piece = field_piece;
+      }
+      
+    }
+
   }
 
 }
