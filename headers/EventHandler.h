285f47088aee29c95f192c2da83ae602a5258940
Yu Huang <y.huang0112@gmail.com>
2022-07-03 15:45:01 +0200

    Taking running bool as reference in handle_events

--- headers/EventHandler.h
+++ headers/EventHandler.h
@@ -8,10 +8,11 @@ class EventHandler {
 private:
   SDL_Event event;
   Board* board;
+  bool is_running;
 public:
   EventHandler(Board* b);
   SDL_Event& get_event();
-  void handle_events(std::vector<int> &possible_moves);
+  void handle_events(std::vector<int> &possible_moves, bool &running);
 };
 
 #endif
