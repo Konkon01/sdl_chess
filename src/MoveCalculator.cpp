45681fb7e437db049d54c7c9aa143bd717f64652
Yu Huang <y.huang0112@gmail.com>
2022-07-04 23:22:48 +0200

    Skeleton implementations

--- src/MoveCalculator.cpp
+++ src/MoveCalculator.cpp
@@ -24,3 +24,27 @@ void MoveCalculator::calculate_moves(int f_ind, char f_piece, std::vector<int> &
     calculate_king(f_ind, f_piece, p_moves);
   }
 }
+
+void MoveCalculator::calculate_pawn(int f_ind, char f_piece, std::vector<int> &p_moves){}
+void MoveCalculator::calculate_rook(int f_ind, char f_piece, std::vector<int> &p_moves){}
+void MoveCalculator::calculate_horse(int f_ind, char f_piece, std::vector<int> &p_moves){}
+void MoveCalculator::calculate_bishop(int f_ind, char f_piece, std::vector<int> &p_moves){}
+void MoveCalculator::calculate_queen(int f_ind, char f_piece, std::vector<int> &p_moves){}
+void MoveCalculator::calculate_king(int f_ind, char f_piece, std::vector<int> &p_moves){}
+  
+bool MoveCalculator::is_same_team(char p1, char p2){
+  return (p1 < 91 && p2 < 91) || (p1 > 91 || p2 > 91);
+}
+
+void MoveCalculator::left_to_right(int f_ind, char f_piece, std::vector<int> &p_moves){
+  //ROOK and QUEEN
+}
+void MoveCalculator::upper_left_to_lower_right(int f_ind, char f_piece, std::vector<int> &p_moves){
+  //BISHOP and QUEEN
+}
+void MoveCalculator::up_to_down(int f_ind, char f_piece, std::vector<int> &p_moves){
+  //ROOK and QUEEN
+}
+void MoveCalculator::lower_left_to_upper_right(int f_ind, char f_piece, std::vector<int> &p_moves){
+  //BISHOP and QUEEN
+}
