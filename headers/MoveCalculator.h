3eccc9c95160ee0e7401c0a5a05ad02fc4769688
Yu Huang <y.huang0112@gmail.com>
2022-07-04 15:17:22 +0200

    Added constructor and separated possible movement calculation of each piece

--- headers/MoveCalculator.h
+++ headers/MoveCalculator.h
@@ -1,10 +1,20 @@
 #ifndef MOVECALCULATOR_H
 #define MOVECALCULATOR_H
+#include "../headers/Board.h"
 #include <vector>
 
 class MoveCalculator {
+  Board *board;
 
+  void calculate_pawn(int f_ind, char f_piece, std::vector<int> &p_moves);
+  void calculate_rook(int f_ind, char f_piece, std::vector<int> &p_moves);
+  void calculate_horse(int f_ind, char f_piece, std::vector<int> &p_moves);
+  void calculate_bishop(int f_ind, char f_piece, std::vector<int> &p_moves);
+  void calculate_queen(int f_ind, char f_piece, std::vector<int> &p_moves);
+  void calculate_king(int f_ind, char f_piece, std::vector<int> &p_moves);
 public:
+  MoveCalculator(Board *b);
+
   void calculate_moves(int f_ind, char f_piece, std::vector<int> &p_moves);
 };
 
--- src/MoveCalculator.cpp
+++ src/MoveCalculator.cpp
@@ -1,5 +1,26 @@
 #include "../headers/MoveCalculator.h"
 
+MoveCalculator::MoveCalculator(Board *b){
+  board = b;
+}
+
 void MoveCalculator::calculate_moves(int f_ind, char f_piece, std::vector<int> &p_moves){
-  
+  if(f_piece == 'P' || f_piece == 'p'){
+    calculate_pawn(f_ind, f_piece, p_moves);
+
+  }else if(f_piece == 'R' || f_piece == 'r'){
+    calculate_rook(f_ind, f_piece, p_moves);
+
+  }else if(f_piece == 'H' || f_piece == 'h'){
+    calculate_horse(f_ind, f_piece, p_moves);
+
+  }else if(f_piece == 'B' || f_piece == 'b'){
+    calculate_bishop(f_ind, f_piece, p_moves);
+
+  }else if(f_piece == 'Q' || f_piece == 'q'){
+    calculate_queen(f_ind, f_piece, p_moves);
+
+  }else if(f_piece == 'K' || f_piece == 'k'){
+    calculate_king(f_ind, f_piece, p_moves);
+  }
 }
